8219d6c5a426e57e04772642cc2e7a8271260f7a
Maciej Bartos <mbartos@griddynamics.com>
2024-06-13 13:34:22 +0200

    feat: setup server

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/user.js";
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
